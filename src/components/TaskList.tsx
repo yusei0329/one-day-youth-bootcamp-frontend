d1f69b15f0dc8cf86be0a63d25196b111861fbdf
yusei0329 <nwys.28@gmail.com>
2021-12-13 16:35:29 +0900

    first commit

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Task } from '../'
+
+type Props = {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+};
+
+export const TaskList: React.VFC<Props> = ({ tasks, setTasks }) => {
+  //Taskの状態を切り替える
+  const handleCheckBox = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    i: number
+  ) => {
+    const newTask = tasks.map((task, _i) => {
+      return _i === i ? { ...task, isDone: e.target.checked } : task;
+    });
+    setTasks(newTask);
+  };
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={`todo-${index}`}>
+          {task.isDone ? <s>{task.label}</s> : task.label}
+          <input 
+            onChange={(e) => handleCheckBox(e, index)}
+            type="checkbox"
+            checked={task.isDone}
+          />
+        </li>
+      ))
+      }
+    </ul>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom";
+import { request } from "./server";
+import { TaskList } from './components/TaskList';
+import { TaskForm } from './components/TaskForm';
+
+// TODOタスクの型
+export type Task = { label: string; isDone: boolean};
+
+const App: React.VFC = () => {
+  // タスクリストを格納する
+  const [tasks, setTasks] = useState<Task[]>([]);
+  // 追加前のタスクを格納する
+  const [newTaskLabel, setNewTaskLabel] = useState<string>('');
+  // ページマウント時にモックAPIからデータを取得
+  useEffect(() => {
+    request.fetchTasks((payload: Task[]) => setTasks(payload) );
+  }, []);
+
+  return (
+    <div style={{ width: '700px', margin: '0 auto'}}>
+      {/* ヘッダー */}
+      <h1>Tutorial Works</h1>
+      <h2>React Todo List</h2>
+      {/* 一覧表示 */}
+      <TaskList {...{ tasks, setTasks }}/>
+      
+      {/* タスク追加、削除 */}
+      <TaskForm {...{ tasks, setTasks, newTaskLabel, setNewTaskLabel }}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
